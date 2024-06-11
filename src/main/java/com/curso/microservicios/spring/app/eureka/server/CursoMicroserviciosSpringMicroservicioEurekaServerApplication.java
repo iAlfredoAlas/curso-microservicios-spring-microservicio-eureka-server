efed0b8b3a1ff1e5e677b74c905313cc3cfff76a
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-11 14:35:09 -0600

    add project files

---
+++ src/main/java/com/curso/microservicios/spring/app/eureka/server/CursoMicroserviciosSpringMicroservicioEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.spring.app.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMicroserviciosSpringMicroservicioEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMicroserviciosSpringMicroservicioEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/microservicios/spring/app/eureka/server/CursoMicroserviciosSpringMicroservicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.spring.app.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMicroserviciosSpringMicroservicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
